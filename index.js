511b3114bfbb7dda71c2d44bfa2660bbeb79fa85
BuildTools <altamiranolucas2013@gmail.com>
2025-05-15 15:09:59 +0200

    Add checkboxes style and add yotube+ menu.

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ document.getElementById('calcstringbutton').onclick = function() {
     document.getElementById('menu').classList.add("hidden");
     document.getElementById('calcstring').classList.remove("hidden");
 };
+document.getElementById('youtubeplusbutton').onclick = function() {
+    document.getElementById('menu').classList.add("hidden");
+    document.getElementById('youtubeplus').classList.remove("hidden");
+};
